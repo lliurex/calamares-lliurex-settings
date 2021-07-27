8e263b4d70171ceb29525e80b43630234047ffdc
daduve <daduve@gmail.com>
2021-07-27 18:54:05 +0200

    Adding 3d print slideshow

---
+++ rsrc/branding/convert_translations.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp /usr/share/calamares/branding/lliurex/lang/calamares-lliurex_ca@valencia.ts /usr/share/calamares/branding/lliurex/lang/calamares-lliurex_ca.ts
+lconvert /usr/share/calamares/branding/lliurex/lang/calamares-lliurex_es.ts -o /usr/share/calamares/branding/lliurex/lang/calamares-lliurex_es.qm
+lconvert /usr/share/calamares/branding/lliurex/lang/calamares-lliurex_ca.ts -o /usr/share/calamares/branding/lliurex/lang/calamares-lliurex_ca.qm
+lconvert /usr/share/calamares/branding/lliurex/lang/calamares-lliurex_ca@valencia.ts -o /usr/share/calamares/branding/lliurex/lang/calamares-lliurex_ca@valencia.qm
---
+++ rsrc/branding/copy_slideshow2git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp ~/ubiquity/21_07/*.svg rsrc/branding/
+cp /usr/share/calamares/branding/lliurex/show.qml ./lliurex/branding/lliurex/
+cp /usr/share/calamares/branding/lliurex/lang/* ./lliurex/branding/lliurex/lang/
+cp /usr/share/calamares/branding/lliurex/*.png ./lliurex/branding/lliurex/

87231f707adbb1ba490081b38399bef4cd97c1c2
daduve <daduve@gmail.com>
2022-07-05 07:21:09 +0200

    deleting svg files in debian package

--- rsrc/branding/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp /home/lliurex/Documentos/lliurex/*.png /usr/share/calamares/branding/lliurex/
-qmlscene -translation /usr/share/calamares/branding/lliurex/lang/calamares-lliurex_ca@valencia.qm -I /usr/share/calamares/qml -geometry 810x485 show.qml

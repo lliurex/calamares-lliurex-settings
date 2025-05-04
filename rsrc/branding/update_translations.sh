fa8c6eacd6900c74bc48bd1721cc77063dd6c890
Raul Rodrigo Segura <raurodse@gmail.com>
2025-05-04 11:19:31 +0200

    clean

--- rsrc/branding/update_translations.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-lupdate show.qml -ts  /usr/share/calamares/branding/lliurex/lang/calamares-lliurex_es.ts
-lupdate show.qml -ts  /usr/share/calamares/branding/lliurex/lang/calamares-lliurex_ca.ts
-lupdate show.qml -ts  /usr/share/calamares/branding/lliurex/lang/calamares-lliurex_ca@valencia.ts
